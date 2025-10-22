5da0399522d9919028aab43072c5443a9336116b
MouseCM <mousecuber@gmail.com>
2025-10-22 13:40:12 +0700

    add random spawn

--- src/main/java/com/abstracts/Brick.java
+++ src/main/java/com/abstracts/Brick.java
@@ -88,24 +88,23 @@ public abstract class Brick extends GameObject {
         int num = rand.nextInt(100);
 
         if (num <= 10) {
-            powerUps.add(new PowerUp(getX(), getY(), "HP"));
+            powerUps.add(new PowerUp(getX() + rand.nextInt((int)(getWidth()-20)), getY(), "HP"));
         }
         else if(num <= 20) {
-            powerUps.add(new PowerUp(getX(), getY(), "FireBall"));
+            powerUps.add(new PowerUp(getX() + rand.nextInt((int)(getWidth()-20)), getY(), "FireBall"));
         }
         else if (num <= 30) {
-            powerUps.add(new PowerUp(getX(), getY(), "x3Ball"));
+            powerUps.add(new PowerUp(getX() + rand.nextInt((int)(getWidth()-20)), getY(), "x3Ball"));
         }
         else if (num <= 40) {
-            powerUps.add(new PowerUp(getX(), getY(), "FastBall"));
+            powerUps.add(new PowerUp(getX() + rand.nextInt((int)(getWidth()-20)), getY(), "FastBall"));
         }
         else if (num <= 50) {
-            powerUps.add(new PowerUp(getX(), getY(), "BigBall"));
+            powerUps.add(new PowerUp(getX() + rand.nextInt((int)(getWidth()-20)), getY(), "BigBall"));
         }
-        else if (num <= 100) {
-            powerUps.add(new PowerUp(getX(), getY(), "BigPaddle"));
+        else if (num <= 60) {
+            powerUps.add(new PowerUp(getX() + rand.nextInt((int)(getWidth()-20)), getY(), "BigPaddle"));
         }
-    }
-
-
+    }   
+    
 }
